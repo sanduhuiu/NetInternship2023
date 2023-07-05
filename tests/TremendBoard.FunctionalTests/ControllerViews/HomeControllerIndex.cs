4f50f61dcfcdbc51253e76ab3456c02913e3ad90
sandu.huiu <sandu.huiu@DESKTOP-QPFFMT6>
2023-07-05 12:20:34 +0300

    1

---
+++ tests/TremendBoard.FunctionalTests/ControllerViews/HomeControllerIndex.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Services.Interfaces;
+using TremendBoard.Mvc.Controllers;
+using TremendBord.Mvc;
+
+namespace TremendBord.FunctionalTests.ControllerViews
+{
+    public class HomeControllerIndex
+    {
+        private CustomWebApplicationFactory<WebMarker> _factory;
+        private HttpClient _client;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _factory = CustomWebApplicationFactory<WebMarker>.Instance();
+            _client = _factory.CreateClient();
+        }
+
+        [Test]
+        public void Index_ReturnsAViewResult()
+        {
+            // Arrange
+            var dateService = new Mock<IDateTime>();
+            dateService.Setup(service => service.Now)
+                .Returns(DateTime.UtcNow);
+
+            var controller = new HomeController(dateService.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public async Task ReturnsViewWithCorrectMessage()
+        {
+            HttpResponseMessage response = await _client.GetAsync("/Home/Index");
+            response.EnsureSuccessStatusCode();
+            string stringResponse = await response.Content.ReadAsStringAsync();
+
+            Assert.IsTrue(stringResponse.Contains("Project Management with Scrum"));
+        }
+    }
+}
