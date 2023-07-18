31d6bb0a7d5d20ddb2f56c5fd451615784b9bcbe
Bianca Negoescu <bianegoe@publicisgroupe.net>
2023-07-18 14:37:12 +0300

    DI

---
+++ src/TremendBoard.Infrastructure.Services/Services/TimeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Services.Interfaces;
+
+namespace TremendBoard.Infrastructure.Services.Services
+{
+    public class TimeService : ITimeService
+    {
+        private DateTime CurrentTime;
+
+        public TimeService() 
+        {
+            CurrentTime = DateTime.Now;
+        }
+
+        public DateTime GetCurrentTime()
+        {
+            return CurrentTime;
+        }
+    }
+}
