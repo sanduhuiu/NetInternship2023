fa278c37dd078f6698d54fe909e23f036d3df35e
andtabac <andrei.tabacaru@publicissapient.com>
2023-07-18 14:40:53 +0300

    Add ITimeService and its implementation

---
+++ src/TremendBoard.Infrastructure.Services/Services/TimeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Services.Interfaces;
+
+namespace TremendBoard.Infrastructure.Services.Services
+{
+    public class TimeService : ITimeService
+    {
+        private readonly IDateTime _time;
+        public TimeService(IDateTime time) 
+        {
+            _time = time;
+        }
+
+
+        public DateTime GetCurrentTime()
+        {
+            return _time.Now;
+        }
+    }
+}
