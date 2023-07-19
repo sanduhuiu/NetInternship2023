88a1dbbe2ee55c1ceed5fcbd62ee75a06667e1fa
andtabac <andrei.tabacaru@publicissapient.com>
2023-07-19 14:04:39 +0300

    Update Project entity

--- src/TremendBoard.Infrastructure.Data/Migrations/20230719110346_UpdateProjectEntity.Designer.cs
+++ src/TremendBoard.Infrastructure.Data/Migrations/20230719110346_UpdateProjectEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TremendBoard.Infrastructure.Data.Context;
 
@@ -11,9 +12,10 @@ using TremendBoard.Infrastructure.Data.Context;
 namespace TremendBoard.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(TremendBoardDbContext))]
-    partial class TremendBoardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230719110346_UpdateProjectEntity")]
+    partial class UpdateProjectEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TremendBoard.Infrastructure.Data/Migrations/20230719110346_UpdateProjectEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TremendBoard.Infrastructure.Data.Migrations
+{
+    public partial class UpdateProjectEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Deadline",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectStatus",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deadline",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectStatus",
+                table: "Projects");
+        }
+    }
+}
